7ddc282de87e7d189501ffe0a2f957d3022a986a
Otavio Fonseca <otavioaf123@gmail.com>
2025-04-08 13:58:12 -0300

    Inicializando Projeto

---
+++ src/test/java/com/example/MotoGearHub/MotoGearHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MotoGearHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoGearHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
